f697843e6a4071138506ab81da78457abae072ec
Danil Kozlov <megacokes@gmail.com>
2018-02-22 16:46:58 +0500

    Transition, works-block пример hover-анимации

--- js/ph-animate.js
+++ js/ph-animate.js
@@ -6,4 +6,15 @@ $(document).ready(function() {
     function() {
       $(this).removeClass('rubberBand');
     }
-)})
+  )
+
+  $('div.animated').hover(
+    function() {
+      $(this).addClass('rubberBand');
+    },
+    function() {
+      $(this).removeClass('rubberBand');
+    }
+  )
+
+})
