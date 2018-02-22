17b59fb0d1cd658cc800612d30e93a004e6d5bb4
Danil Kozlov <megacokes@gmail.com>
2018-02-22 15:39:57 +0500

    Animate ph on hover with js

---
+++ js/ph-animate.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('img.animated').hover(
+    function() {
+      $(this).addClass('rubberBand');
+    },
+    function() {
+      $(this).removeClass('rubberBand');
+    }
+)})
