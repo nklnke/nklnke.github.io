34eb7752b6a4b398e2137542679309e31a0236f7
Danil Kozlov <megacokes@gmail.com>
2018-03-13 15:05:05 +0500

    lil minify

--- js/to-the-stars.js
+++ js/to-the-stars.js
@@ -6,21 +6,21 @@ $(document).ready(function() {
   // fade in #to-the-stars
   $(function () {
 
-      $(window).scroll(function () {
-          if ($(this).scrollTop() > 100) {
-              $('#to-the-stars').fadeIn();
-          } else {
-              $('#to-the-stars').fadeOut();
-          }
-      });
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#to-the-stars').fadeIn();
+      } else {
+        $('#to-the-stars').fadeOut();
+      }
+    });
 
-      // scroll to 0px on click
-      $('#to-the-stars a').click(function () {
-          $('body,html').animate({
-              scrollTop: 0
-          }, 250);
-          return false;
-      });
+    // scroll to 0px on click
+    $('#to-the-stars a').click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 250);
+      return false;
+    });
       
   });
 
